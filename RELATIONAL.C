bb5030b13001080a7c3a33b01e0a5aacec86d9b4
KRISHNA-WASU <wasukrishna220@gmail.com>
2024-10-27 23:06:04 +0530

    Add files via upload

--- RELATIONAL.C
+++ RELATIONAL.C
@@ -1,17 +1,22 @@
-// C program to illustrate the relational operators
+// Programm  for relational operator
 #include <stdio.h>
 
-int main()
-{
-
-    int a = 25, b = 5;
-
-    printf("a < b  : %d\n", a < b);
-    printf("a > b  : %d\n", a > b);
-    printf("a <= b: %d\n", a <= b);
-    printf("a >= b: %d\n", a >= b);
-    printf("a == b: %d\n", a == b);
-    printf("a != b : %d\n", a != b);
-
+int main() {
+    int a,b;
+    a=25;
+    b=5 ;
+    (a>b);//a is greater than b
+    printf("%d\n",a>b);
+    (a<b);//b is greater than a
+    printf("%d\n",a<b);
+    (a==b);//a is equal to b
+    printf("%d\n",a==b);
+    (a>=b);//a is greater than equal to b
+    printf("%d\n",a>=b);
+    (a<=b);//b is greater than equal to a
+    printf("%d\n",a<=b);
+    (a!=b);//a is not equal to b
+    printf("%d\n",a!=b);
+    
     return 0;
 }
